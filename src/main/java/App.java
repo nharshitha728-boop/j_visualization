e4f8c1fdc3334e22eae865f0fac9196f0c226724
Harshitha <nharshitha728@gmail.com>
2026-02-14 11:14:57 +0530

    Add App class with hello world functionality

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+package com.experiment;
+public class App {
+    public String sayHello() { return "Hello Jenkins!"; }
+    public static void main(String[] args) {
+        System.out.println(new App().sayHello());
+    }
+}
